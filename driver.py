4c222d71f9a4bd2ba85ffe6b50fb983b4274bba7
pr0n1s <redstrom83@gmail.com>
2018-02-19 23:37:13 -0600

    Refactor

--- driver.py
+++ driver.py
@@ -8,13 +8,24 @@ import angr
 proj = angr.Project('./bo')
 
 print "[*] Simulation in progress: Detecting buffer overflow"
+print "[*] *************************************************"
 sm = proj.factory.simulation_manager(save_unconstrained=True)
 
+ss = []
 while len(sm.unconstrained) == 0:
 	sm.step()
+	ss.append(sm.stashes['active'])
 
-us = sm.unconstrained[0]
-crash = us.posix.dumps(0)
-print "[*] Simulation completed: Vulnerable"
-print "[*] Seg Fault @: {}".format(us)
-print "[*] Input length: {}\n[*] Input: {}".format(len(repr(crash).split('\\'))-1 ,repr(crash))
+crash = sm.unconstrained[0].posix.dumps(0)
+
+print "[*] Simulation completed"
+print "[*] ********************"
+print "[*] Seg Fault @: {}".format(simstates[-2])
+print "[*] -------------------------------------"
+print "[*] Trace:"
+print "[*] ------"
+for s in ss:
+	print "\t{}".format(s)
+print "[*] Input length: {}".format(len(repr(crash).split('\\'))-1)
+print "[*] -----------------"
+print "[*] Input: {}".format(repr(crash))
