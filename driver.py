1c437bb6b8783b9acc9f5473918afcd0d025b766
pr0n1s <redstrom83@gmail.com>
2018-02-25 00:46:39 -0600

    Updated

--- driver.py
+++ driver.py
@@ -17,33 +17,30 @@ def getblock(proj, entry):
 	block = proj.factory.block(entry)
 	return block
 
-def getsegfault(ss):
-	return ss[-2]
+def getaddr(ss):
+	return str(ss[-2][0]).split('@')[1].split('>')[0]
 
 def getinputlen(crash):
 	return len(repr(crash).split('\\'))-1
 
-def display(crash, ss):
-  print "[*] Simulation completed"
-  print "[*] ********************"
-  print "[*] Seg Fault @: {}".format(getsegfault(ss))
-  print "[*] -------------------------------------"
-  print "[*] Trace:"
-  print "[*] ------"
-  for s in ss:
-    print "\t{}".format(s)
-  print "[*] Input length: {}".format(getinputlen(crash))
-  print "[*] -----------------"
-  print "[*] Input: {}".format(repr(crash))
+def display(proj, crash, ss):
+	print "\nSimulation completed"
+	print "********************"
+	print "\nSeg Fault @: {}".format(getaddr(ss))
+	print "-----------------------"
+	print getblock(proj, int(getaddr(ss), 16)).pp()
+	print "\n[*] Input length: {}".format(getinputlen(crash))
+	print "---------------------"
+	print "Input: {}".format(repr(crash))
 
 def main():
 	proj = angr.Project('./bo')
 	sm = proj.factory.simulation_manager(save_unconstrained=True)
-
-	print "[*] Simulation in progress: Detecting buffer overflow"
-	print "[*] *************************************************"
+	print "*************************************************"
+	print "Simulation in progress: Detecting buffer overflow"
+	print "*************************************************"
 	crash, ss = dowork(sm)
-	display(crash, ss)
+	display(proj, crash, ss)
 
 if __name__ == '__main__':
 	logging.getLogger('angr').setLevel('ERROR')
